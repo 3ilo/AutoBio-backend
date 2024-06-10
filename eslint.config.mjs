6a73490c848fdb57684b0e7cf8df4cfe5eacab01
milochase13 <milochase13@gmail.com>
2024-06-10 01:10:21 -0700

    initial login backend

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// eslint.config.js
+import stylisticTs from '@stylistic/eslint-plugin-ts'
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      '@stylistic/ts': stylisticTs
+    },
+    rules: {
+      '@stylistic/ts/indent': ['error', 2],
+    }
+  }
+]
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const User = require('./models/User');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const withAuth = require('./middleware/withAuth.ts');
+
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.get('/api/home', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.get('/api/secret', withAuth, function(req, res) {
+  res.send('The password is potato');
+});
+
+// Check if token is valid
+app.get('/checkToken', withAuth, function(req, res) {
+    res.sendStatus(200);
+  });
+
+// POST route to register a user
+app.post('/api/register', function(req, res) {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  user.save().then((user) => {
+    res.status(200).send("Welcome to the club!");
+  }).catch((error) => {
+    res.status(500).send("Error registering new user please try again.");
+  })
+});
+
+// POST route to authenticate a user
+app.post('/api/authenticate', function(req, res) {
+  const { email, password } = req.body;
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      res.status(401)
+        .json({
+          error: 'Incorrect email or password'
+        });
+    }
+    else{
+      user.isCorrectPassword(password, function(err, same) {
+        if (err) {
+          res.status(500)
+            .json({
+              error: 'Internal error please try again'
+            });
+        } else if (!same) {
+          res.status(401)
+            .json({
+              error: 'Incorrect email or password'
+            });
+        } else {
+          // Issue token
+          const payload = { email };
+          const token = jwt.sign(payload, process.env.AUTH_SECRET, {
+            expiresIn: '1h'
+          });
+          res.cookie('token', token, { httpOnly: true })
+            .sendStatus(200);
+        }
+      });
+    }
+  }).catch((err, user) => {
+    console.log(err);
+    res.status(500)
+      .json({
+        error: 'Internal error please try again1'
+      });
+  });
+});
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
